1c7b51dfa030d9b15573dff333c02717db192865
polyphant1 <chrisl1991@hotmail.co.uk>
2017-06-23 14:25:55 +0100

    update pickle script

--- Cloudy_output/pickle_sed.py
+++ Cloudy_output/pickle_sed.py
@@ -3,13 +3,11 @@ import numpy as np
 import pickle
 
 out_dir = 'output/'
+c = 2.99792458e8
 
 models = ['BC03_Padova1994_Salpeter_lr','BPASSv2_imf135all_100','BPASSv2_imf135all_100-bin','FSPS_default_Salpeter','M05_Salpeter_rhb','P2_Salpeter_ng']
 
-c = 2.99792458e8
 
-
-# convert to L_{\nu}
 def cloudy_unit_conversion(m, wavelength):
     """
     Cloudy output units: nu Lnu / (4 pi r^2)
@@ -21,51 +19,35 @@ def cloudy_unit_conversion(m, wavelength):
     return(m)
 
 
-
 for model in models:
 
     model_pickle = '../Input_SPS/model_pickles/'+model+'.p'
 
-    # --- load pickled sed
-
-    print('Loading '+model+' SED pickle')
-
+    print 'Loading', model, ' SED pickle'
     data = pickle.load(open(model_pickle,'rb'))#, encoding='latin1')
 
-    # sed = data['SED']
     Z = data['metallicities']
     ages = data['ages']
 
-    ### load post-cloudy obscured sed
-
-    # find all wavelength values across all metallicities and ages
-    print('Finding wavelength values')
-
+    print 'Finding wavelength values'
     wavelengths = np.array([])
 
     for i,z in enumerate(Z):
         for j,a in enumerate(ages):
             
-            lam = np.loadtxt(model+'/'+str(i)+'_'+str(j)+'.cont', delimiter='\t', usecols = [0])
-            
+            lam = np.loadtxt(model+'/'+str(i)+'_'+str(j)+'.cont', delimiter='\t', usecols = [0]) 
             wavelengths = np.unique(np.concatenate([wavelengths,lam]))
 
 
     wavelengths = np.sort(wavelengths)[::-1]
 
-    # convert to 1e-10 m
-    # wavelengths *= 1e4
-
-    print wavelengths
-
-    # save sed values for each metallicity and age
     print 'Saving SED'
 
     cloudy_sed = np.zeros((len(Z),len(ages)), dtype=np.ndarray)
 
     for i,z in enumerate(Z):
         for j,a in enumerate(ages):
-    
+            
             lam, flambda = np.loadtxt(model+'/'+str(i)+'_'+str(j)+'.cont', delimiter='\t', usecols = [0,4]).T
         
             # create an empty sed array 
@@ -78,16 +60,11 @@ for model in models:
             cloudy_sed[i,j] = cloudy_unit_conversion(sed, wavelengths*1e-10)
 
 
-    print cloudy_sed
-
     print 'Pickle post-cloudy SED'
-
     data = {'SED': cloudy_sed,  # Lnu
             'Metallicity': Z,   
             'Age': ages,  # Myr
             'Wavelength': wavelengths}  # 1e10 m
 
     pickle.dump(data,open(out_dir+model+'.p','wb'), protocol=2)
-
-
 
