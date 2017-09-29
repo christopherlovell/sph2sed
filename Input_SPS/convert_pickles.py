c208d39887a23fd5652bd51de90b79af6f27adac
polyphant1 <chrisl1991@hotmail.co.uk>
2017-09-29 22:17:07 +0100

    fixed unit error in pickle conversion

--- Input_SPS/convert_pickles.py
+++ Input_SPS/convert_pickles.py
@@ -11,17 +11,19 @@ models = ['BC03_Padova1994_Salpeter_lr', 'BPASSv2_imf135all_100', 'BPASSv2_imf13
 
 for model in models:
 
-  print model
+  print(model)
 
-  data = pcl.load(open('model_pickles/'+model+'.p','rb'))
+  with open("model_pickles/%s.p"%model, 'rb') as f:
+    data = pcl.load(f)#, encoding='latin1')
   
   out = {}
   out['Age'] = data['ages']   # Myr
-  out['SED'] = data['SED']  # Lnu / L_odot
-  out['SED'] /= 3.827E33   # Lnu
+  out['SED'] = data['SED']  # Lnu [W Hz^-1]
+  out['SED'] /= 1e7  # Lnu [erg s^-1 Hz^-1]  
 
   out['Metallicity'] = data['metallicities']   # M_odot yr^-1
-  out['Wavelength'] = data['lam'] * 1e4  # 1e10 m 
-  
-  pcl.dump(out, open('pickles/'+model+'.p', 'w'))
+  out['Wavelength'] = data['lam'] * 1e4  # AA 
+ 
+  with open("pickles/%s.p"%model, 'wb') as f:
+    pcl.dump(out, f)
 
